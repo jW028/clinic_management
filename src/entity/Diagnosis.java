d756004d0e9b6cc6e652deeab22e09aa0327f702
JwTan <tanjw05@gmail.com>
2025-07-14 12:27:23 +0800

    adt reference

---
+++ src/entity/Diagnosis.java
@@ -0,0 +1,51 @@
+package entity;
+
+public class Diagnosis {
+    private String id;
+    private String name;
+    private String description;
+    private String severity;
+
+    public Diagnosis (String id, String name, String severity) {
+        this.id = id;
+        this.name = name;
+        this.severity = severity;
+    }
+
+    // Getters 
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getSeverity() {
+        return severity;
+    }
+
+    // Setters
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setSeverity(String severity) {
+        this.severity = severity;
+    }
+
+    // Display
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Name: ").append(name).append("\n")
+          .append("Description: ").append(description).append("\n")
+          .append("Severity: ").append(severity).append("\n");
+        return sb.toString();
+    }
+
+}
